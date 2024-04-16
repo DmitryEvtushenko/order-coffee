323120fade080fbb2738b71edf75243d054eafb4
LegendaV <115637903+LegendaV@users.noreply.github.com>
2024-04-16 18:36:32 +0500

    1 5

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const originForm = document.querySelector('.beverage').cloneNode(true);
 let counter = 1;
+let factCount = 1;
 
 const addForm = document.querySelector('.add-button');
-
 function newForm(event) {
     const form = originForm.cloneNode(true);
     form.querySelector('.beverage-count').innerText = `Напиток №${++counter}`;
-    document.insertBefore(form, addForm);
+    let clonedRadioButtons = form.querySelectorAll('input[type="radio"]');
+    clonedRadioButtons.forEach((radio, index) => {
+        radio.setAttribute('name', `milk${counter}`);
+    });
+    factCount++;
+    addForm.before(form);
 }
-
 addForm.addEventListener('click', newForm);
 
 const submitButton = document.querySelector('.submit-button');
 function submitFunction(event) {
     event.preventDefault();
+    document.querySelector('.modalTitle').innerText = `Вы заказали ${factCount} напит${factCount % 10 === 1 ? 'ок' : ([2, 3, 4].includes(factCount % 10) && Math.floor(factCount / 10) % 10 !== 1) ? 'кa' : 'ков'}`
     document.querySelector('.modalWindow').style.display = 'block';
 }
 submitButton.addEventListener('click', submitFunction);
